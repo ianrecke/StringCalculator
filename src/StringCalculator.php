8fbd4b434bb0c7df5f6dfd8da2f0e52fe0c60612
ianrecke <ian.recke@gmail.com>
2022-02-27 19:12:42 +0100

    adding functions and order

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -12,7 +12,15 @@ class StringCalculator
         if($this->emptyString($input_string))
             return "0";
         else{
-            return strval($this->addNumbers($input_string));
+            $errors = $this->errorLogs($input_string);
+            if ($this->emptyString($errors))
+                if ($this->hasCustomOperator($input_string)){
+                    return strval($this->addWithCustomSeparator($input_string));
+                }else{
+                    return strval($this->addNewLineSeparator($input_string));
+                }
+            else
+                return $errors;
         }
     }
     private function emptyString(string $input_string):bool{
@@ -27,6 +35,7 @@ class StringCalculator
         return array_sum($FloatNumbers);
     }
 
+
     private function addNewLineSeparator(string $input_string):float{
         if ($this->emptyString($input_string) == "0"){
             return "0";
@@ -36,10 +45,28 @@ class StringCalculator
             return $this->addNumbers($newStr);
         }
     }
+    //contains the function errors
+    private function errorLogs(string $input_string):string{
+        $errorLog = "";
+        if($this->invalidNewLineSeparator( $input_string)){
+            $pos = strpos($input_string,",\n");
+            $errorLog.="Number expected but '\\n' found at position ".$pos.".\n";
+        }
+        if($this->missingNumberLastPosition($input_string))
+            $errorLog.="Number expected but EOF found.\n";
+        if($this->hasCustomOperator($input_string)){
+            if($this->addWithCustomSeparatorRestriction($input_string)){
+                $separator = $this->createCustomSeparators($input_string);
+                $newStr = str_replace("\n", ",", $input_string);
+                $pos = strpos($newStr,",");
+                $errorLog.= $separator." expected but ',' found at position ".$pos.".\n";
+            }
+        }
+    }
 
     private function invalidNewLineSeparator(string $input_string):bool{
         $newStr = str_replace(" ","",$input_string);
-        if (str_contains($input_string,",\n")||str_contains($input_string,"\n,"))
+        if (str_contains($input_string,",\n"))
             return true;
         else
             return false;
@@ -48,18 +75,40 @@ class StringCalculator
     private function missingNumberLastPosition(string $input_string):bool{
         $delimiters = ["\n",","];
         $newStr = str_replace($delimiters, $delimiters[1], $input_string);
-        if(str_ends_with($input_string,","))
+        if(str_ends_with($newStr,","))
+            return true;
+        else
+            return false;
+    }
+
+    public function hasCustomOperator(string $input_string):bool{
+        if(str_starts_with($input_string,"//"))
             return true;
         else
             return false;
     }
 
     private function createCustomSeparators(string $input_string):string{
-        if(str_starts_with($input_string,"//")){
-            //contents is split in 2, first half contains the separator
-            $contents= explode("\n",$input_string);
-            return substr($contents[0],2);
-        }
-        return false;
+        //contents is split in 2, first half contains the separator
+        $contents= explode("\n",$input_string);
+        return substr($contents[0],2);
     }
+
+    private function addWithCustomSeparatorRestriction(string $input_string):bool{
+        $separator = $this->createCustomSeparators($input_string);
+        if(str_contains($input_string,",")||str_contains($input_string,"\n")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    private function addWithCustomSeparator(string $input_string):float{
+
+        $separator = $this->createCustomSeparators($input_string);
+        $FloatNumbers = array_map('floatval',explode(''.$separator, $input_string));
+        return array_sum($FloatNumbers);
+
+    }
+
+
 }
