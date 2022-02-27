d760c5a871c25db5985fac8bc1d130f3566cc664
ianrecke <ian.recke@gmail.com>
2022-02-27 19:26:32 +0100

    error tests

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 namespace Deg540\PHPTestingBoilerplate\Test;
 use Deg540\PHPTestingBoilerplate\StringCalculator;
 use PHPUnit\Framework\TestCase;
+use function Sodium\add;
 
 class StringCalculatorTest extends TestCase
 {
@@ -28,10 +29,34 @@ class StringCalculatorTest extends TestCase
     }
 
     /**
-     * @test : check if string is empty
+     * @test : check if string is addable
      */
     public function addNumbersTest(){
         $res = $this->stringCalculator->add("1,2");
         $this->assertEquals("3",$res); //assert
     }
+
+    /**
+     * @test : check if separator is valid
+     */
+    public function createValidCustomSeparator(){
+        $res = $this->stringCalculator->hasCustomOperator("/sep\n");
+        $this->assertEquals(false,$res); //assert
+    }
+    /**
+     * @test : check if input is valid
+     */
+    public function ErrorMessageExpectedNumber()
+    {
+        $res = $this->stringCalculator->add("1,\n2");
+        $this->assertEquals("Number expected but '\\n' found at position 2.\n",$res); //assert
+    }
+    /**
+     * @test : check if input is valid
+     */
+    public function ErrorMessageExpectedNumberButEof(){
+        $res = $this->stringCalculator->add("1,2,");
+        $this->assertEquals("Number expected but EOF found.\n",$res); //assert
+    }
 }
+
