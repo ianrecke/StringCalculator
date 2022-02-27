52c53501ebd372eae793929a1ddfd5d6bd3bd597
ianrecke <ian.recke@gmail.com>
2022-02-27 17:59:55 +0100

    test_with_functions_empty

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -10,7 +10,7 @@ class StringCalculator
         if($this->emptyString($input_string))
             return "0";
         else{
-            
+            return "String with content";
         }
     }
     private function emptyString(string $input_string):bool{
@@ -20,31 +20,39 @@ class StringCalculator
             return false;
     }
 
-    public function stringToFloat(string $input_string):float{
-        if ($this->emptyString($input_string) == "0"){
-            return "0";
-        }else{
-            return floatval($input_string);
-        }
+    private function addNumbers(string $input_string):float{
+        $FloatNumbers = array_map('floatval',explode(',', $input_string));
+        return array_sum($FloatNumbers);
     }
 
-    public function addNumbers(string $input_string):float{
-        if ($this->emptyString($input_string) == "0"){
-            return "0";
-        }else{
-            $FloatNumbers = array_map('floatval',explode(',', $input_string));
-            return array_sum($FloatNumbers);
-        }
-    }
-
-    public function addNewLineSeparator(string $input_string):float{
+    private function addNewLineSeparator(string $input_string):float{
         if ($this->emptyString($input_string) == "0"){
             return "0";
         }else{
             $delimiters = ["\n",","];
             $newStr = str_replace($delimiters, $delimiters[1], $input_string);
-            $FloatNumbers = array_map('floatval',explode(',', $newStr));
-            return array_sum($FloatNumbers);
+            return $this->addNumbers($newStr);
         }
+    }
+
+    private function invalidNewLineSeparator(string $input_string):bool{
+        $newStr = str_replace(" ","",$input_string);
+        if (str_contains($input_string,",\n")||str_contains($input_string,"\n,"))
+            return true;
+        else
+            return false;
+    }
+
+    private function missingNumberLastPosition(string $input_string):bool{
+        $delimiters = ["\n",","];
+        $newStr = str_replace($delimiters, $delimiters[1], $input_string);
+        if(str_ends_with($input_string,","))
+            return true;
+        else
+            return false;
+    }
+
+    private function createCustomSeparators(string $input_string):string{
+
     }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -1,11 +1,34 @@
 <?php
 
-namespace koans\StringCalculator\tests;
 
-use koans\StringCalculator\src\StringCalculator;
+
+declare(strict_types=1);
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+use Deg540\PHPTestingBoilerplate\StringCalculator;
 use PHPUnit\Framework\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
 
+    /**
+     * @setUp
+     */
+    protected function setUp():void{
+        parent::setUp();
+        $this->stringCalculator=  new StringCalculator();
+    }
+
+    /**
+     * @test : check if string is empty
+     */
+    public function string_is_empty(){
+        $res = $this->stringCalculator->add("");
+        $this->assertEquals("0",$res); //assert
+    }
+
+   
+    public function addNumbersTest(){
+        $res = $this->stringCalculator->addNumbers("");
+    }
 }
